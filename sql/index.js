4ac64ae84b91e5e61e072b47d35296c2b6182dbc
CalistaIo <e0426281@u.nus.edu>
2020-10-16 22:05:38 +0800

    Add queries

--- sql/index.js
+++ sql/index.js
@@ -1,32 +1,24 @@
 const sql = {}
 
 sql.query = {
-	// Counting & Average
-	count_play: 'SELECT COUNT(winner) FROM game_plays WHERE user1=$1 OR user2=$1',
-	count_wins: 'SELECT COUNT(winner) FROM game_plays WHERE winner=$1',
-	avg_rating: 'SELECT AVG(rating) FROM user_games INNER JOIN game_list ON user_games.gamename=game_list.gamename WHERE username=$1',
 	
-	// Information
-	page_game: 'SELECT * FROM game_list WHERE ranking >= $1 AND ranking <= $2 ORDER BY ranking ASC',
-	page_lims: 'SELECT * FROM game_list ORDER BY ranking ASC LIMIT 10 OFFSET $1',
-	ctx_games: 'SELECT COUNT(*) FROM game_list',
-	all_games: 'SELECT ranking,game_list.gamename AS game,rating FROM user_games INNER JOIN game_list ON user_games.gamename=game_list.gamename WHERE username=$1 ORDER BY ranking ASC',
-	all_plays: 'SELECT gamename AS game, user1, user2, winner FROM game_plays WHERE user1=$1 OR user2=$1',
-	
-	// Insertion
-	add_game: 'INSERT INTO user_games (username, gamename) VALUES($1,$2)',
-	add_play: 'INSERT INTO game_plays (user1, user2, gamename, winner) VALUES($1,$2,$3,$4)',
-	add_user: 'INSERT INTO username_password (username, password, status, first_name, last_name) VALUES ($1,$2,\'Bronze\',$3,$4)',
-	
-	// Login
-	userpass: 'SELECT * FROM username_password WHERE username=$1',
-	
-	// Update
-	update_info: 'UPDATE username_password SET first_name=$2, last_name=$3 WHERE username=$1',
-	update_pass: 'UPDATE username_password SET password=$2 WHERE username=$1',
-	
-	// Search
-	search_game: 'SELECT * FROM game_list WHERE lower(gamename) LIKE $1',
+	insert_bid: 'SELECT insert_bid($1, $2, $3, $4, $5, $6, $7, $8)',
+
+	choose_bids: 'SELECT choose_bids()',
+
+	rate_or_review: 'SELECT rate_or_review($1, $2, $3, $4, $5, $6, $7)',
+
+	set_transac_details: 'SELECT set_transac_details($1, $2, $3, $4, $5, $6, $7)',
+
+	pay_bid: 'SELECT pay_bid($1, $2, $3, $4, $5)',
+
+	search_reviews: 'SELECT review FROM Bids WHERE username = $1 AND review IS NOT NULL',
+
+	search_avg_rating: 'SELECT AVG(rating) FROM Bids WHERE username = $1',
+
+	search_past_orders: 'SELECT pet_name, p_start_date, p_end_date, username, rating, review, payment_method, mode_of_transfer, is_paid, total_price FROM Bids WHERE owner_username = $1 AND is_successful = true',
+
+	search_petdays: 'SELECT SUM(duration) FROM (SELECT p_end_date - p_start_date + 1 AS duration FROM Bids WHERE username = $1 AND p_start_date >= $2 AND p_start_date <= $3 AND is_successful = true)',
 }
 
 module.exports = sql

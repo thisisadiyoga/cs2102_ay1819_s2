6f6acea294e17b04b73841da73975eb64a1477ae
CalistaIo <e0426281@u.nus.edu>
2020-10-27 18:27:09 +0800

    Update sql code

--- sql/postgre.sql
+++ sql/postgre.sql
@@ -1,18 +1,18 @@
 CREATE TABLE Timings (
-	p_start_date DATE,
-	p_end_date DATE,
+	p_start_date TIMESTAMP,
+	p_end_date TIMESTAMP,
 	PRIMARY KEY (p_start_date, p_end_date),
-	CHECK (p_end_date >= p_start_date)
+	CHECK (p_end_date > p_start_date)
 )
 
 CREATE TABLE Bids (
 	owner_username VARCHAR,
 	pet_name VARCHAR,
-	p_start_date DATE,
-	p_end_date DATE,
-	starting_date DATE,
-	ending_date DATE,
-	username VARCHAR,
+	p_start_date TIMESTAMP,
+	p_end_date TIMESTAMP,
+	starting_date TIMESTAMP,
+	ending_date TIMESTAMP,
+	caretaker_username VARCHAR,
 	rating NUMERIC,
 	review VARCHAR,
 	is_successful BOOLEAN,
@@ -21,52 +21,24 @@ CREATE TABLE Bids (
 	is_paid BOOLEAN,
 	total_price NUMERIC NOT NULL CHECK (total_price > 0),
 	type_of_service VARCHAR NOT NULL,
-	PRIMARY KEY (pet_name, owner_username, p_start_date, p_end_date, starting_date, ending_date, username),
+	PRIMARY KEY (pet_name, owner_username, p_start_date, p_end_date, starting_date, ending_date, caretaker_username),
 	FOREIGN KEY (p_start_date, p_end_date) REFERENCES Timings(p_start_date, p_end_date),
-	FOREIGN KEY (starting_date, ending_date, username) REFERENCES Availabilities(starting_date, ending_date,
-	username),
+	FOREIGN KEY (starting_date, ending_date, caretaker_username) REFERENCES Availabilities(starting_date, ending_date,
+	caretaker_username),
 	FOREIGN KEY (pet_name, owner_username) REFERENCES ownsPets(name, username),
-	UNIQUE (pet_name, owner_username, username, p_start_date, p_end_date),
+	UNIQUE (pet_name, owner_username, caretaker_username, p_start_date, p_end_date),
 	CHECK ((is_successful = true) OR (rating IS NULL AND review IS NULL)),
 	CHECK ((is_successful = true) OR (payment_method IS NULL AND is_paid IS NULL AND
 	mode_of_transfer IS NULL)),
 	CHECK ((rating IS NULL) OR (rating >= 0 AND rating <= 5)),
-	CHECK ((p_start_date >= starting_date) AND (p_end_date <= ending_date) AND (p_end_date >= p_start_date))
+	CHECK ((p_start_date >= starting_date) AND (p_end_date <= ending_date) AND (p_end_date > p_start_date))
 )
 
-CREATE OR REPLACE PROCEDURE insert_bid(ou VARCHAR, pn VARCHAR, ps DATE, pe DATE, sd DATE, ed DATE, ct VARCHAR, ts VARCHAR) AS
+CREATE OR REPLACE PROCEDURE insert_bid(ou VARCHAR, pn VARCHAR, ps TIMESTAMP, pe TIMESTAMP, sd TIMESTAMP, ed TIMESTAMP, ct VARCHAR, ts VARCHAR) AS
 $$ DECLARE tot_p NUMERIC;
 BEGIN
-tot_p := (pe - ps + 1) * (SELECT daily_price FROM Charges WHERE username = ct AND cat_name IN (SELECT cat_name FROM ownsPets WHERE username = ou AND name = pn));
+tot_p := (EXTRACT(DAY FROM AGE(pe, ps)) + 1) * (SELECT daily_price FROM Charges WHERE username = ct AND cat_name IN (SELECT cat_name FROM ownsPets WHERE username = ou AND name = pn));
 INSERT INTO Bids VALUES (ou, pn, ps, pe, sd, ed, ct, NULL, NULL, NULL, NULL, NULL, NULL, tot_p, ts);
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE PROCEDURE choose_bids() AS
-$$ BEGIN
-UPDATE Bids SET is_successful = (CASE WHEN random() < 0.5 THEN true ELSE false END)
-WHERE is_successful IS NULL;
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE PROCEDURE rate_or_review(rat NUMERIC, rev VARCHAR, ou VARCHAR, pn VARCHAR, ct VARCHAR, ps DATE, pe DATE) AS
-$$ BEGIN
-UPDATE Bids SET rating = rat, review = rev WHERE owner_username = ou AND pet_name = pn AND
-username = ct AND p_start_date = ps AND p_end_date = pe;
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE PROCEDURE set_transac_details(pm VARCHAR, mot VARCHAR, ou VARCHAR, pn VARCHAR, ct VARCHAR, ps DATE, pe DATE) AS
-$$ BEGIN
-UPDATE Bids SET payment_method = pm, mode_of_transfer = mot WHERE owner_username = ou AND pet_name = pn AND
-username = ct AND p_start_date = ps AND p_end_date = pe;
-END; $$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE PROCEDURE pay_bid(ou VARCHAR, pn VARCHAR, ct VARCHAR, ps DATE, pe DATE) AS
-$$ BEGIN
-UPDATE Bids SET is_paid = true WHERE owner_username = ou AND pet_name = pn AND username = ct AND
-p_start_date = ps AND p_end_date = pe;
 END; $$
 LANGUAGE plpgsql;
 

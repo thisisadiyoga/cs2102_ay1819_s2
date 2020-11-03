cead4137311cbb647e8e9b911a29168914aad6b7
Joanna-YJA <joannajoules@gmail.com>
2020-11-03 18:28:05 +0800

    Add caretaker calendar

--- auth/init.js
+++ auth/init.js
@@ -14,9 +14,6 @@ const pool = new Pool({
     //ssl: true
     user: postgres_details.user,
 	database: postgres_details.database,
-	host: postgres_details.host,
-	port: postgres_details.port,
-	password: postgres_details.password,
 	 idleTimeoutMillis: 2000
 });
 

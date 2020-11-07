f6b390744d724c8c7a7b41adb83c2b34dac8cdb8
Nikhilalalalala <uvnikhi@gmail.com>
2020-11-07 23:10:07 +0800

    Add stuff

--- public/seed/Category.sql
+++ public/seed/Category.sql
@@ -4,3 +4,15 @@ INSERT INTO Categories VALUES ('Kitten', 5.5);
 INSERT INTO Categories VALUES ('Puppy', 6);
 INSERT INTO Categories VALUES ('Fowl', 3);
 
+insert into Administrators (admin_username, password) values ('ccommuzzo0', 'fS5qCNwfQeZ');
+insert into Administrators (admin_username, password) values ('etarling1', 'ak2HqHBWiid');
+insert into Administrators (admin_username, password) values ('bcahani2', 'eYf3UI9L');
+insert into Administrators (admin_username, password) values ('sdeport3', 'QP7Jlk9t0');
+insert into Administrators (admin_username, password) values ('ztarbett4', 'WZ1bSUAFMS');
+insert into Administrators (admin_username, password) values ('sboddam5', '2vYzSCxd');
+insert into Administrators (admin_username, password) values ('ahouldin6', 'jOc0QYEfCBy');
+insert into Administrators (admin_username, password) values ('bgraeber7', 'HoPzGF');
+insert into Administrators (admin_username, password) values ('fbauldry8', '0CdwN0jdI1r');
+insert into Administrators (admin_username, password) values ('rbowlands9', 'cBh8VTIQXGD');
+insert into Administrators (admin_username, password) values ('kberthona', 'TfT6MpGv8');
+

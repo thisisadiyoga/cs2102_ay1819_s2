bb2a0a7d391f1dc82a6b29769b5a14e30cc1042c
CalistaIo <e0426281@u.nus.edu>
2020-11-10 16:39:02 +0800

    Change transaction form

---
+++ public/javascripts/insert_transac.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $("#startdate").flatpickr({
+         enableTime: true,
+         dateFormat: "Y-m-d H:i:00",
+         minDate: new Date()
+    });
+    
+    $("#enddate").flatpickr({
+         enableTime: true,
+         dateFormat: "Y-m-d H:i:00",
+         minDate: new Date()
+    });
+});

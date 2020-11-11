52c7356d8c1cf3c0f17ea5c9a30cc6be86dd1682
xseh <39018497+xseh@users.noreply.github.com>
2020-11-11 19:21:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const postgres_details = {
 	host : 'ec2-54-228-170-125.eu-west-1.compute.amazonaws.com',
 	database : 'd77amq3smn9npd', 
 	password : '9bd38cda8c65382de924af4937714349a62eaf2038e01988dd5be687dce69c85', 
+	port : 5432, 
 }
     
 module.exports = postgres_details
